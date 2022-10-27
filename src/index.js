c34f99e936cdd0c3be62d1932ce2cfc740d53865
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-27 13:19:52 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(<App />, document.querySelectorAll("#root"));
+ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );

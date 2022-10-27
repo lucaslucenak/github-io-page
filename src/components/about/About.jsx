3461f8ed0bb62a4a71305925626c4a4650d8f8b8
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-27 17:20:45 -0300

    Add about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,53 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/img/me-about.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FaUniversity} from 'react-icons/fa'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get to Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src= {ME} alt="about image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>1+ Years Working</small>
+            </article>
+
+            <article className="about__card">
+              <FaUniversity className='about__icon'/>
+              <h5>Student from UEPB</h5>
+              <small>CRA: 9.03</small>
+            </article>
+
+            <article className="about__card">
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>10+ Completed</small>
+            </article>
+          </div>
+
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac pharetra mi, vitae sagittis nulla.
+             Integer dolor enim, rhoncus in consectetur quis, mattis eu lorem. Ut tempus, libero a suscipit fermentum,
+              justo est pulvinar neque, ut maximus nisl mauris sed dui. In hac habitasse platea dictumst.
+               Pellentesque sed auctor lacus. Mauris molestie dapibus lorem non consequat.</p>
+
+               <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 

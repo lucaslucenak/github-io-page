64a4760c8dfef19388c5f09869538966317ebd0d
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-27 14:53:30 -0300

    Bug fix at Header section

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import CV from '../../assets/pdf/cv.pdf'
+import './header.css'
 
 const CTA = () => {
   return (
-    <div className="cta">
+    <div className='cta'>
         <a href={CV} className="btn" download>Download CV</a>
         <a href="#contact" className="btn btn-primary">Contact me</a>
     </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ import HeaderSocials from './HeaderSocials'
 const Header = () => {
   return (
     <header>
-      <container className="header__container">
+      <div className="container header__container">
         <h5>Hey, I'm</h5>
         <h1>Lucas Lucena</h1>
         <h5 className="text-light">Backend Developer</h5>
@@ -19,7 +19,7 @@ const Header = () => {
         </div>
 
         <a href="#contact" className="scroll__down">Scroll Down</a>
-      </container>
+      </div>
     </header>
   )
 }

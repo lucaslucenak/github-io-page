127d40193dfef838825f8ea18d69e9ed8331f0e4
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-28 00:17:57 -0300

    Add Services

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,64 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id="services">Services</section>
+    <section id="services">
+      <h5>What I can Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>API Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Service */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Process Automations</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Service */}
+
+      </div>
+    </section>
   )
 }
 

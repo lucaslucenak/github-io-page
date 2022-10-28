107d8ab5246505fad19e69963355890973b12b71
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-28 15:31:31 -0300

    Add footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './footer.css'
+import {BsLinkedin} from 'react-icons/bs'
+import {BsInstagram} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="" className="footer__logo"></a>
+
+      <ul className="permalinks">
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Testimonials</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.linkedin.com/in/lucas-lucena-6588271a3/"><BsLinkedin/></a>
+        <a href="https://github.com/lucaslucenak/"><FaGithub/></a>
+        <a href="https://www.instagram.com/lucaslucenak/"><BsInstagram/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; EGATOR Tutorials. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 

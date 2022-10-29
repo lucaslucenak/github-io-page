37a363821edb16e80c8c9214b52f28ae416ee3a2
Lucas Lucena <lucas.lucenak@gmail.com>
2022-10-29 16:07:54 -0300

    Update footer and nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -17,7 +17,7 @@ const Nav = () => {
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
       <a href="#professional" onClick={() => setActiveNav('#professional')} className={activeNav === '#professional' ? 'active' : ''}><MdWork/></a>
-      <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a>
+      {/* <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a> */}
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )

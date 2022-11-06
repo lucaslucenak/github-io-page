815c32d91a768f57353122d7e4292fd2c5b3d611
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-05 23:32:18 -0300

    Update nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -19,7 +19,7 @@ const Nav = () => {
       <a href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBook/></a>
       <a href="#professional" onClick={() => setActiveNav('#professional')} className={activeNav === '#professional' ? 'active' : ''}><MdWork/></a>
       <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><GrProjects/></a>
-      <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><GrGroup/></a>
+      {/* <a href="#testimonials" onClick={() => setActiveNav('#testimonials')} className={activeNav === '#testimonials' ? 'active' : ''}><GrGroup/></a> */}
       {/* <a href="#services" onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><RiServiceLine/></a> */}
       <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>

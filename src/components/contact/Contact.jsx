3de8ad5298f4299b9f410e31ef268efea968d61f
Lucas Lucena <lucas.lucenak@gmail.com>
2022-11-06 21:27:57 -0300

    Update Contact section

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,21 +33,21 @@ const Contact = () => {
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
             <h5>lucas.lucenak@gmail.com</h5>
-            <a href="mailto:lucas.lucenak@gmail.com" target="_blank">Send a message</a>
+            <a href="mailto:lucas.lucenak@gmail.com" target="_blank">Send me an e-mail</a>
           </article>
 
           <article className="contact__option">
             <BsLinkedin className='contact__option-icon'/>
             <h4>Linkedin</h4>
             <h5>/in/lucas-lucena-6588271a3/</h5>
-            <a href="https://www.linkedin.com/in/lucas-lucena-6588271a3/" target="_blank">Send a message</a>
+            <a href="https://www.linkedin.com/in/lucas-lucena-6588271a3/" target="_blank">Connect with me</a>
           </article>
 
           <article className="contact__option">
             <FaWhatsapp className='contact__option-icon'/>
             <h4>WhatsApp</h4>
             <h5>+55 83 98690-7270</h5>
-            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5583986907270" target="_blank">Send a message</a>
+            <a href="http://api.whatsapp.com/send?1=pt_BR&phone=5583986907270" target="_blank">Send me a message</a>
           </article>
         </div>
 
